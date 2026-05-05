53eaa3b4e32295acb9529038f410b1713c599a91
khatri harshi <khatriharshi2012@gmail.com>
2026-05-05 16:54:37 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 const noteRoute = require("./routes/notes.routes");
 const userRoute = require("./routes/user.routes");
 const todoRoute = require("./routes/todo.routes");
@@ -37,19 +37,26 @@ mongoose
   .catch((err) => console.log(err));
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("Incoming Origin:", origin);
-
-    // allow requests without origin (like Postman, mobile apps)
-    if (!origin) return callback(null, true);
+  origin(origin, callback) {
+    // Allow tools like Postman or same-origin server requests.
+    if (!origin) {
+      return callback(null, true);
+    }
 
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
 
-    return callback(new Error(`CORS blocked: ${origin}`));
+    if (!isProduction && localDevOriginPattern.test(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error(`CORS blocked for origin: ${origin}`));
   },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
